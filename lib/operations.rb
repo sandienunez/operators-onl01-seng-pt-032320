09d266ace7773ec6c97f84f02201ee182e546ef9
Sandie Nunez <snunez@vt.edu>
2020-03-30 04:16:09 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,14 @@
 def unsafe?(speed)
+  if speed > 60 || speed < 40
+    true
+  elsif speed < 60 && speed > 40
+    false
+  end
 
 end
 
-
-
 def not_safe?(speed)
+	(speed > 60 || speed < 40) ? true : false
 	
 end
 	
